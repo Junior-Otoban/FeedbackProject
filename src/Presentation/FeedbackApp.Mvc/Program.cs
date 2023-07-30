821c68e22fa4574f979c43954180a6f877641694
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-07-30 17:50:33 +0300

    hangfire

--- src/Presentation/FeedbackApp.Mvc/Program.cs
+++ src/Presentation/FeedbackApp.Mvc/Program.cs
@@ -4,6 +4,8 @@ using FeedbackApp.Infrastructure.Repositories;
 using FeedbackApp.Infrastructure.Repositories.EntityFramework;
 using FeedbackApp.Services.Mapping;
 using FeedbackApp.Services.Services.Feedback;
+using Hangfire;
+using Hangfire.PostgreSql;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +23,14 @@ var connectionString = builder.Configuration.GetConnectionString("FeedBack");
 builder.Services.AddEntityFrameworkNpgsql().AddDbContext<FeedBackContext>(opt =>
         opt.UseNpgsql(connectionString));
 
+var hangfireCS = builder.Configuration.GetConnectionString("Hangfire");
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<HangfireContext>(opt =>
+        opt.UseNpgsql(hangfireCS));
+
+builder.Services.AddHangfire(configuration =>
+        configuration.UsePostgreSqlStorage(builder.Configuration.GetConnectionString("Hangfire")));
+builder.Services.AddHangfireServer();
+
 var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
@@ -29,6 +39,8 @@ var services = scope.ServiceProvider;
 var feedBackContext = services.GetRequiredService<FeedBackContext>();
 feedBackContext.Database.EnsureCreated();
 
+var hangfireContext = services.GetRequiredService<HangfireContext>();
+hangfireContext.Database.EnsureCreated();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -44,7 +56,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseHangfireDashboard();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
