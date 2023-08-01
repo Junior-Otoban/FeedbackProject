d2c67e752776ec3cba4b0a88aa602910686d51fa
Sümeyye <65611401+smybayrktr@users.noreply.github.com>
2023-08-01 10:59:36 +0300

    Delete Desktop/Tech_Hub/Tech_Hub directory

--- Desktop/Tech_Hub/Tech_Hub/src/Presentation/Tech_Hub.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tech_Hub.WebUI.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
